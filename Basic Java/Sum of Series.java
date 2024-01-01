6386808c862fb68735c97bd2133b0e26bac859e8
Drishti Rai <76700390+drishtirai@users.noreply.github.com>
2024-01-01 22:48:56 +0530

    Create Sum of Series.java

---
+++ Basic Java/Sum of Series.java
@@ -0,0 +1,10 @@
+// User function Template for Java
+
+class Solution {
+    // function to return sum of  1, 2, ... n
+    long seriesSum(int n) {
+        // code here
+        n = (n+1)/2;
+        return n;
+    }
+}
