5d79359684a4a95921afd5fc1186712fb414f91f
Drishti Rai <76700390+drishtirai@users.noreply.github.com>
2024-01-01 22:51:39 +0530

    Update Sum of Series.java

--- Basic Java/Sum of Series.java
+++ Basic Java/Sum of Series.java
@@ -2,7 +2,7 @@
 
 class Solution {
     // function to return sum of  1, 2, ... n
-    long seriesSum(int n) {
+    long seriesSum(long n) {
         // code here
         n = (n+1)/2;
         return n;
